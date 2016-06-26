cefaecc4d04f7fdb9e5002bd41b94041efcfd88f
Zach Deibert <zachdeibert@gmail.com>
2016-06-26 11:20:54 -0500

    Fixed Shadow constructor

--- Framework/Graphics/Shadow.ts
+++ Framework/Graphics/Shadow.ts
@@ -4,7 +4,7 @@ namespace Framework.Graphics {
         public blur: number;
         public offset: Point;
 
-        public Shadow(color: Color, blur: number = 10, offset?: Point) {
+        public constructor(color: Color, blur: number = 10, offset?: Point) {
             this.color = color;
             this.blur = blur;
             this.offset = offset || new Point();
