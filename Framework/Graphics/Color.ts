d6e0efa803a3ba5a4a1a6414deea8a86ae4985d3
Zach Deibert <zachdeibert@gmail.com>
2016-06-26 11:20:45 -0500

    Fixed Color constructor

--- Framework/Graphics/Color.ts
+++ Framework/Graphics/Color.ts
@@ -9,7 +9,7 @@ namespace Framework.Graphics {
             return "rgba(" + this.r + ", " + this.g + ", " + this.b + ", " + this.a + ")";
         }
 
-        public Color(r?: number, g?: number, b?: number, a?: number) {
+        public constructor(r?: number, g?: number, b?: number, a?: number) {
             this.r = r || 0;
             this.g = g || 0;
             this.b = b || 0;
